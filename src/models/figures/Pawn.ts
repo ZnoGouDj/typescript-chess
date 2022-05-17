fc749b910e445328b89939db8f70c743f5178fda
ZnoGouDj <znogoud3@gmail.com>
2022-05-17 20:50:27 +0200

    Add queen moves + parental moves

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -10,4 +10,11 @@ export class Pawn extends Figure {
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.PAWN;
   }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+    return true;
+  }
 }
--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -10,4 +10,14 @@ export class Queen extends Figure {
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.QUEEN;
   }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+    if (this.cell.isEmptyVertical(target)) {
+      return true;
+    }
+    return false;
+  }
 }
