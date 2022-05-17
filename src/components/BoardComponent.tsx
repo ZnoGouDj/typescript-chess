e28f529a29d592fdbd22402134af5889bf41a45c
ZnoGouDj <znogoud3@gmail.com>
2022-05-17 17:55:25 +0200

    Add available moves highlight, add motion functionality

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
 import CellComponent from './CellComponent';
 
 interface BoardProps {
@@ -8,12 +9,42 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    } else {
+      setSelectedCell(cell);
+    }
+  }
+
+  useEffect(() => {
+    highlightCells();
+  }, [selectedCell]);
+
+  function highlightCells() {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
+
   return (
     <div className="board">
       {board.cells.map((row, index) => (
         <React.Fragment key={index}>
           {row.map(cell => (
-            <CellComponent cell={cell} key={cell.id} />
+            <CellComponent
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+            />
           ))}
         </React.Fragment>
       ))}
