41a26c35f3b70f08538a5c076ff4e8ca041b7655
ZnoGouDj <znogoud3@gmail.com>
2022-05-17 15:37:51 +0200

    Create start figures positions

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,11 @@
 import { Cell } from './Cell';
 import { Colors } from './Colors';
+import { Pawn } from './figures/Pawn';
+import { Bishop } from './figures/Bishop';
+import { King } from './figures/King';
 import { Queen } from './figures/Queen';
+import { Knight } from './figures/Knight';
+import { Rook } from './figures/Rook';
 
 export class Board {
   cells: Cell[][] = [];
@@ -23,7 +28,50 @@ export class Board {
     return this.cells[y][x];
   }
 
+  private addBishops() {
+    new Bishop(Colors.BLACK, this.getCell(2, 0));
+    new Bishop(Colors.BLACK, this.getCell(5, 0));
+    new Bishop(Colors.WHITE, this.getCell(2, 7));
+    new Bishop(Colors.WHITE, this.getCell(5, 7));
+  }
+
+  private addKings() {
+    new King(Colors.BLACK, this.getCell(4, 0));
+    new King(Colors.WHITE, this.getCell(4, 7));
+  }
+
+  private addKnights() {
+    new Knight(Colors.BLACK, this.getCell(1, 0));
+    new Knight(Colors.BLACK, this.getCell(6, 0));
+    new Knight(Colors.WHITE, this.getCell(1, 7));
+    new Knight(Colors.WHITE, this.getCell(6, 7));
+  }
+
+  private addPawns() {
+    for (let i = 0; i < 8; i++) {
+      new Pawn(Colors.BLACK, this.getCell(i, 1));
+      new Pawn(Colors.WHITE, this.getCell(i, 6));
+    }
+  }
+
+  private addQueens() {
+    new Queen(Colors.BLACK, this.getCell(3, 0));
+    new Queen(Colors.WHITE, this.getCell(3, 7));
+  }
+
+  private addRooks() {
+    new Rook(Colors.BLACK, this.getCell(0, 0));
+    new Rook(Colors.BLACK, this.getCell(7, 0));
+    new Rook(Colors.WHITE, this.getCell(0, 7));
+    new Rook(Colors.WHITE, this.getCell(7, 7));
+  }
+
   public addFigures() {
-    new Queen(Colors.WHITE, this.getCell(3, 3));
+    this.addBishops();
+    this.addKings();
+    this.addKnights();
+    this.addPawns();
+    this.addQueens();
+    this.addRooks();
   }
 }
